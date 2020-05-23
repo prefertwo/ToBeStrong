a543ca1141c7174646e2bc8eef802df1fea46bfb
prefertwo <1522876658@qq.com>
2020-05-23 12:07:32 +0800

    new record

--- miniprogram/pages/new/new.js
+++ miniprogram/pages/new/new.js
@@ -12,8 +12,46 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    const dateT = new Date().formate("yyyy-MM-dd")
+    console.log(dateT);
 
+    this.setData({
+      currentDate: dateT
+    })
   },
+
+  // 提交
+  submitRecord: function(e) {
+    console.log(e);
+    const values = e.detail.value;
+    if(!values.theme) {
+      return wx.showToast({
+        title: '填写个合适的主题吧',
+        icon: 'none'
+      })
+    }
+
+    wx.showToast({
+      title: '发布成功啦',
+    })
+  },
+
+  // 上传图片
+  uploadImage: function() {
+    wx.chooseImage({
+      count: 9,
+      sizeType: ['original'],
+      sourceType: ['album', 'camera'],
+      complete: (res) => {
+        console.log(res);
+        const tempFilePaths = res.tempFilePaths; // 图片临时路径
+        this.setData({
+          images: tempFilePaths
+        })
+      },
+    })
+  },
+  
 
   /**
    * 生命周期函数--监听页面初次渲染完成
